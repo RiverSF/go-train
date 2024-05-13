612b2df0b3d0bc66d9d0b398756372b6bb1e976b
River <river_sf@163.com>
2024-05-13 16:14:22 +0800

    struct 如何判空

---
+++ train/struct.go
@@ -0,0 +1,30 @@
+package train
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Pe struct {
+	age int
+}
+
+func (x Pe) IsStructureEmpty() bool {
+	return reflect.DeepEqual(x, Pe{})
+}
+
+func main() {
+	x := Pe{}
+
+	if x.IsStructureEmpty() {
+		fmt.Println("Structure is empty")
+	} else {
+		fmt.Println("Structure is not empty")
+	}
+}
+
+/**
+如何判断一个结构体为空
+
+https://www.includehelp.com/golang/how-to-check-if-structure-is-empty.aspx
+*/
